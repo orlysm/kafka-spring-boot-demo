1a0dd2cb3f85550924dc23f3db53befab33b8f32
Orly Moshe <orlysam5@gmail.com>
2020-08-27 16:32:55 +0300

    move to config package

--- src/main/java/kafka/demo/config/KafkaConsumerConfiguration.java
+++ src/main/java/kafka/demo/config/KafkaConsumerConfiguration.java
@@ -1,4 +1,4 @@
-package kafka.demo;
+package kafka.demo.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
--- src/main/java/kafka/demo/config/KafkaProducerConfiguration.java
+++ src/main/java/kafka/demo/config/KafkaProducerConfiguration.java
@@ -1,4 +1,4 @@
-package kafka.demo;
+package kafka.demo.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
