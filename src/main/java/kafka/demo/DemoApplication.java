04352477824471b6ee54092e63b376faf8864fe2
Orly Moshe <orlysam5@gmail.com>
2020-08-27 15:37:36 +0300

    publish text msg to Kafka

--- src/main/java/kafka/demo/DemoApplication.java
+++ src/main/java/kafka/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package kafka.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+
+	public DemoApplication(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+		kafkaTemplate.send("Example","Message");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
