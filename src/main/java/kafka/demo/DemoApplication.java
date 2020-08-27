6187851d2ff368ef59da3f6ae9e2215c9fa333ba
Orly Moshe <orlysam5@gmail.com>
2020-08-27 15:57:41 +0300

    publish Object msg

--- src/main/java/kafka/demo/DemoApplication.java
+++ src/main/java/kafka/demo/DemoApplication.java
@@ -7,15 +7,30 @@ import org.springframework.kafka.core.KafkaTemplate;
 @SpringBootApplication
 public class DemoApplication {
 
-	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final KafkaTemplate<String, Object> kafkaTemplate;
 
-	public DemoApplication(KafkaTemplate<String, String> kafkaTemplate) {
+	public DemoApplication(KafkaTemplate<String, Object> kafkaTemplate) {
+
 		this.kafkaTemplate = kafkaTemplate;
-		kafkaTemplate.send("Example","Message");
+		TestMsg testMsg = new TestMsg();
+		kafkaTemplate.send("Example",testMsg);
+
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+	}
+	
+	private static class TestMsg{
+		
+		public String id = "123";
+
+		public String firstName = "Orly";
+
+		public String lastName = "M";
+
 	}
 
 }
