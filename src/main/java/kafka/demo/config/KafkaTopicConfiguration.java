5427ef80a902f4c0cafd2dc7d2e43e5ae936fa19
Orly Moshe <orlysam5@gmail.com>
2020-09-08 15:53:29 +0300

    add partitions and 2 consumer groups

---
+++ src/main/java/kafka/demo/config/KafkaTopicConfiguration.java
@@ -0,0 +1,19 @@
+package kafka.demo.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfiguration {
+
+    @Bean
+    public NewTopic exampleTopic() {
+        return TopicBuilder.name("example")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+}
--- src/main/java/kafka/demo/service/KafkaConsumerService.java
+++ src/main/java/kafka/demo/service/KafkaConsumerService.java
@@ -1,13 +1,33 @@
 package kafka.demo.service;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
 
-    @KafkaListener(topics = "Example", groupId = "group1")
-    public void consumer(String message) {
-        System.out.println(message);
+    @KafkaListener(
+            groupId = "group1",
+            topicPartitions = @TopicPartition(topic = "example", partitions = "0"),
+            containerFactory = "concurrentKafkaListenerContainerFactory1")
+    public void consumerA(String message) {
+        System.out.println("T:" + Thread.currentThread().getId() + " Group:1, Partitions:0     Message:" + message);
+    }
+
+    @KafkaListener(
+            groupId = "group1",
+            topicPartitions = @TopicPartition(topic = "example", partitions = { "1", "2" }),
+            containerFactory = "concurrentKafkaListenerContainerFactory1")
+    public void consumerB(String message) {
+        System.out.println("T:" + Thread.currentThread().getId() + " Group:1, Partitions:1,2   Message:" + message);
+    }
+
+    @KafkaListener(
+            groupId = "group2",
+            topics = "example",
+            containerFactory = "concurrentKafkaListenerContainerFactory2")
+    public void consumerC(String message) {
+        System.out.println("T:" + Thread.currentThread().getId() + " Group:2, Partitions:0,1,2 Message:" + message);
     }
 }
